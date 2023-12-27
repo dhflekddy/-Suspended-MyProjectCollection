800685b6097200e7b5c9d21a46c2833a39d40d5b
hyojin <dhflekddy@hanmail.net>
2023-12-27 20:23:32 +0900

    v1

---
+++ MyProject_/src/main/java/WednesdayProject/MyProject/controller/MemberForm.java
@@ -0,0 +1,12 @@
+package WednesdayProject.MyProject.controller;
+
+public class MemberForm {
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
---
+++ MyProject_/src/main/java/WednesdayProject/MyProject/repository/MemberRepository.java
@@ -0,0 +1,18 @@
+package WednesdayProject.MyProject.repository;
+
+import WednesdayProject.MyProject.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+
+@Repository
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member>findById(Long id);
+    Optional<Member>findByName(String name);
+
+    List<Member> findAll();
+}
+
+
